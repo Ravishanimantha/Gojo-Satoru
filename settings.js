ae2d01a958b4ad468605dfd1056b8dd03c07ec10
Ravishanimantha <109650770+Ravishanimantha@users.noreply.github.com>
2022-07-20 16:40:55 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,17 +19,17 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['94775150685']
+global.premium = ['94775150685']
+global.ownernomer = '94775150685'
+global.ownername = '🐦puncha aiya'
+global.botname = 'puncha aiya'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
+global.myweb = 'https://chat.whatsapp.com/FjXrJyJmUoaIpaTqFgeNTV'
+global.packname = 'පුන්නෙ අයියා'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
